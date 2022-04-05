f1c361c878fc467cfae28d6552fd1ad4148de8ed
gjeon03 <gjeon0321@gmail.com>
2022-04-05 19:56:35 +0900

    fix cpp00/ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
-#include <string>
 #include <cctype>
-
-void print_upper(char *str)
-{
-	int i;
-	std::string strUp;
-
-	i = 0;
-	while (str[i])
-	{
-		strUp += std::toupper(str[i]);
-		i++;
-	}
-	std::cout << strUp;
-}
 
 int main(int argc, char **argv)
 {
 	int i;
+	int j;
 
 	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+		i = 0;
+		while (++i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+				std::cout << (char)std::toupper(argv[i][j++]);
+		}
 	}
-	i = 0;
-	while (++i < argc)
-		print_upper(argv[i]);
 	std::cout << std::endl;
 	return (0);
 }
