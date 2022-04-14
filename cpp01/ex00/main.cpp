aef9c9ff82fdabe3b45c002f5783961ebf387e74
gjeon03 <gjeon0321@gmail.com>
2022-04-14 19:15:36 +0900

    cpp01/ex00

---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+int main()
+{
+	Zombie zombieStack = Zombie("stack");
+	Zombie *zombieHeap = new Zombie("heap");
+	Zombie *zombieNew = newZombie("newZombie");
+
+	zombieStack.announce();
+	zombieHeap->announce();
+	zombieNew->announce();
+	randomChump("randomChump");
+	delete zombieHeap;
+}
