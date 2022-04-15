527663046e54006cf32fb019b31008c6c87103c5
Gyeongyeon Jeon <gjeon@c2r2s8.42seoul.kr>
2022-04-15 17:19:51 +0900

    make cpp01/ex03

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,20 +6,17 @@ int main()
 	std::string *stringPTR = &str;
 	std::string &stringREF = str;
 
+	std::cout << "display the memory address" << std::endl;
 	std::cout << "===================================" << std::endl;
-	std::cout << " display the address of the string " << std::endl;
-	std::cout << "===================================" << std::endl;
-	std::cout << "by using str      : " << &str << std::endl;
-	std::cout << "by using stringPTR: " << stringPTR << std::endl;
-	std::cout << "by using stringREF: " << &stringREF << std::endl
-			  << std::endl;
+	std::cout << "str      : " << &str << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl << std::endl;
 
+	std::cout << "display the string" << std::endl;
 	std::cout << "====================================" << std::endl;
-	std::cout << "         display the string         " << std::endl;
-	std::cout << "====================================" << std::endl;
-	std::cout << "by using str      : " << str << std::endl;
-	std::cout << "by using stringPTR: " << *stringPTR << std::endl;
-	std::cout << "by using stringREF: " << stringREF << std::endl;
+	std::cout << "str      : " << str << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
 
 	return 0;
 }
